54e6f0a56790f8e7113f4a6ec5a97d2fa149c8cd
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-02-15 00:09:53 -0430

    Enabled render function for Polls index view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -12,7 +12,7 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
