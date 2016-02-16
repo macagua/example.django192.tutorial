8483ace577a658d19db7c45df53ce7c8e3c08d45
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-02-16 11:25:39 -0430

    Added the was_published_recently() method in list_display option for Question model in Admin interface

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,7 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
