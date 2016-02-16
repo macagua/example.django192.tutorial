74201e081b64293a9f626dc5a2b21c25417941cf
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-02-16 11:32:17 -0430

    Adedd list_filter option for filter the Question list by pub_date field

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -13,6 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
     ]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
