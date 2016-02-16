8fb39b91478adf3921b64fa5723efe85b3343069
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-02-16 11:05:30 -0430

    Added related objects between Choice and Question models in Admin interface for Polls app

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
