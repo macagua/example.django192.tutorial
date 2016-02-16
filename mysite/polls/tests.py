e13177e545718017315bd92886ec0e6ae1fcd2bc
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-02-16 10:22:00 -0430

    Added more improvements about QuestionMethodTests class

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -17,7 +17,8 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text,
                                    pub_date=time)
-1
+
+
 class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
@@ -29,23 +30,25 @@ class QuestionMethodTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
 
-def test_was_published_recently_with_old_question(self):
-    """
-    was_published_recently() should return False for questions whose
-    pub_date is older than 1 day.
-    """
-    time = timezone.now() - datetime.timedelta(days=30)
-    old_question = Question(pub_date=time)
-    self.assertEqual(old_question.was_published_recently(), False)
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
 
-def test_was_published_recently_with_recent_question(self):
-    """
-    was_published_recently() should return True for questions whose
-    pub_date is within the last day.
-    """
-    time = timezone.now() - datetime.timedelta(hours=1)
-    recent_question = Question(pub_date=time)
-    self.assertEqual(recent_question.was_published_recently(), True)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
+
 
 class QuestionViewTests(TestCase):
     def test_index_view_with_no_questions(self):
