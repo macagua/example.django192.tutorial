1f48ff07b48664d5883624e2da2e456e24a75ca4
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-02-16 11:34:11 -0430

    Adedd search_fields option for search the Questions objects by question_text field

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -14,6 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
